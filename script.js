c7cf72c5e583001ea7166a020fc47998aace0b87
Yacian <omsyacian@gmail.com>
2022-04-24 15:38:01 -0400

    added a playGame function and changed computerPlay function

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-function computerPlay() {
-    var computerSelection = ["rock", "paper", "scissors"];
-    const answer = Math.floor(Math.random() * computerSelection.length);
-    console.log("Computer selected " + computerSelection[answer]);
-}
-userSelection = window.prompt("Enter rock paper or scissors: ", "")
-if (userSelection == "rock")
+const rpsArray = ["rock", "paper", "scissors"];
+let computerSelection;
+let userSelection;
+
+
+function computerPlay()
 {
-    console.log("User selected rock")
-}
-else if (userSelection == "paper")
-{
-    console.log("user selected paper")
-}
-else if (userSelection == "scissors")
-{
-    console.log("user selected scissors")
+    computerSelection = Math.floor(Math.random() * rpsArray.length);
+    console.log("computer has selected " + rpsArray[computerSelection])
 }
 
 computerPlay()
+
+function playGame() {
+    userSelection = userSelection.toLowerCase();
+    if (rpsArray[computerSelection] == userSelection)
+    {
+        console.log("It's a tie!")
+    }
+    else if 
+    (
+        (rpsArray[computerSelection] == "rock" && userSelection == "scissors") ||
+        (rpsArray[computerSelection] == "scissors" && userSelection == "paper") ||
+        (rpsArray[computerSelection] == "paper" && userSelection == "rock")
+    )
+    {
+        console.log("Computer wins!");
+    }
+    else
+    {
+        console.log("You win!")
+    }
+}
+
+userSelection = window.prompt("Enter rock, paper or scissors: ", "")
+
+playGame()
